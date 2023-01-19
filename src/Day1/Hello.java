f05a52a6986ec3fa7ad8f8db167c5777b3165019
Gop Arop <gop.arop@yahoo.com>
2023-01-19 11:37:46 -0500

    Updated after messing around with Java syntax.

--- src/Day1/Hello.java
+++ src/Day1/Hello.java
@@ -1,7 +1,7 @@
 package Day1;
 
 public class Hello {
-
+// Exactly one of those classes must contain a main function
     //starting point for the code to run
     public static void main(String[] args) {
         System.out.println("Hello World");
