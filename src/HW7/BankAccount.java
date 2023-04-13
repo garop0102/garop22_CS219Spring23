256cb6b67ec3ecc045826801630a7f0915b7c38e
Gop Arop <gop.arop@yahoo.com>
2023-04-13 19:08:41 -0400

    Homework 7

--- src/HW7/BankAccount.java
+++ src/HW7/BankAccount.java
@@ -1,6 +1,111 @@
 package HW7;
 
+import java.util.ArrayList;
+
 public class BankAccount {
-    private String FirstN, LastN, City,State, Street,Zipcode;
+    private String FirstN, LastN; // first and last name of holder
+    private String AccNum; // account number
+    private String Street,City,State,ZipCode; // address for holder
+    private double Balance; // account balance for holder
+    private double Interest; // interest rate
+
+
+    public BankAccount(String firstN, String lastN, String accNum
+            , String street, String city, String state, String zipCode, double balance, double interest) {
+        FirstN = firstN;
+        LastN = lastN;
+        AccNum = accNum;
+        Street = street;
+        City = city;
+        State = state;
+        ZipCode = zipCode;
+        Balance = balance;
+        Interest = interest;
+    }
+
+    public String getFirstN() {
+        return FirstN;
+    }
+
+    public String getLastN() {
+        return LastN;
+    }
+
+    public String getAccNum() {
+        return AccNum;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public String getZipCode() {
+        return ZipCode;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public double getInterest() {
+        return Interest;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "FirstN='" + FirstN + '\'' +
+                ", LastN='" + LastN + '\'' +
+                ", AccNum='" + AccNum + '\'' +
+                ", Street='" + Street + '\'' +
+                ", City='" + City + '\'' +
+                ", State='" + State + '\'' +
+                ", ZipCode='" + ZipCode + '\'' +
+                ", Balance=" + Balance +
+                ", Interest=" + Interest +
+                '}';
+    }
+
+
+    // adds to the balance amount
+    public double credit(double that){
+        Balance = this.Balance + that;
+        return Balance;
+    }
+
+
+    // takes away from the balance amount
+    public double debit(double that){
+        Balance = this.Balance - that;
+        if (Balance < 0)
+            return 0;
+        else
+            return Balance;
+    }
+
+    // takes away number of interest from balance amount
+    public double addInterest(){
+        Balance = this.Balance + this.Interest;
+        return Balance;
+    }
+
+    // changes the interest rate
+    public double setInterestRate(double rate){
+        this.Interest = rate;
+        return rate;
+    }
+
+
+
+
+
 
 }
--- src/HW7/Main.java
+++ src/HW7/Main.java
@@ -1,5 +1,44 @@
 package HW7;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount("Gop","Arop","180939772","528 Boyden Street",
+                "Syracuse","NY","13206",2034.04,4);
+        BankAccount b2 =  new BankAccount("John","Smith","203456794"
+                , "20 Park Road","Salt Lake City","UT","84101",1039.20,5);
+        BankAccount b3 = new BankAccount("Will","Smith","589549093"
+                , "1207 Skittles Ave","Los Angeles","CA","90032",3240596.50,8);
+
+
+        System.out.println(b1);
+        System.out.println(b1.credit(50));
+        System.out.println(b1.debit(30));
+        System.out.println(b1.debit(3000) == 0.0);
+
+        System.out.println(b2);
+        System.out.println(b2.credit(150.20));
+        System.out.println(b2.debit(75.60));
+        System.out.println(b2.debit(2000) == 0.0);
+
+        System.out.println(b3);
+        System.out.println(b3.credit(700.03));
+        System.out.println(b3.debit(90.34));
+        System.out.println(b3.debit(50000000) == 0.0);
+
+        ArrayList<BankAccount> accounts = new ArrayList<>();
+        accounts.add(b1);
+        accounts.add(b2);
+        accounts.add(b3);
+        displayAccounts(accounts);
+
+    }
+
+    public static void displayAccounts(ArrayList<BankAccount> accounts){
+        for (BankAccount account : accounts) System.out.println(account);
+
+    }
+
 
 }
